361e5b8d1f327eca37d0afe0cf567a88c3a6f2a0
irina <obesyuk@gmail.com>
2024-06-17 21:45:13 +0300

    function adjusted

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -5,10 +5,10 @@ function solveEquation(a, b, c) {
 	let d = Math.pow(b, 2) - 4 * a * c;
 
 	if (d < 0) {
-		return;
+		return [];
 	} else if (d === 0) {
 		let root = -b / (2 * a);
-		return root;
+		return [root];
 	} else if (d > 0) {
 		let root1 = (-b + Math.sqrt(d)) / (2 * a);
 		let root2 = (-b - Math.sqrt(d)) / (2 * a);
